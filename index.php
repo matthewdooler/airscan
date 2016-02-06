3b1013240530e7a51215805ff89f5b0b5be6636a
Matthew Dooler <dooler.matthew@gmail.com>
2016-02-06 16:08:06 +0000

    generate filename

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 echo '<h1>AirScan</h1>';
 
-$timestamp = date('Y-m-d\TH:i:s.Z\Z', time());
+$timestamp = date('Y-m-d_H-i-s', time());
 $filename = "AirScan_" + $timestamp;
 $scanner = "hpaio:/usb/Officejet_J4500_series?serial=CN9C7D10MW052T";
 
-echo $timestamp;
+echo $filename;
 
 ?>
