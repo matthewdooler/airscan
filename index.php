2a1a04af7b29f25482aaa0cc20a7a1fcf34a12c7
Matthew Dooler <dooler.matthew@gmail.com>
2016-02-06 16:20:06 +0000

    do the scanning

--- index.php
+++ index.php
@@ -11,6 +11,15 @@ $timestamp = date('Y-m-d_H-i-s', time());
 $filename = "AirScan_" . $timestamp;
 $scanner = "hpaio:/usb/Officejet_J4500_series?serial=CN9C7D10MW052T";
 
-echo $filename;
+if(isset($_POST)) {
+	$email = $_POST["email"]; // TODO: remember to escape this before using it
+	if(strlen($email) > 0) {
+		shell_exec("scanimage -d $scanner --format tiff > $filename.tiff");
+		shell_exec("convert $filename.tiff $filename.pdf");
+		unlink("$filename.tiff");
+	} else {
+		echo "Enter an email address";
+	}
+}
 
 ?>
