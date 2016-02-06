3787bc7e5cee52632078f4d73c9f96dd6ee8ab01
Matthew Dooler <dooler.matthew@gmail.com>
2016-02-06 16:21:47 +0000

    do the scanning

--- index.php
+++ index.php
@@ -14,9 +14,13 @@ $scanner = "hpaio:/usb/Officejet_J4500_series?serial=CN9C7D10MW052T";
 if(isset($_POST)) {
 	$email = $_POST["email"]; // TODO: remember to escape this before using it
 	if(strlen($email) > 0) {
-		shell_exec("scanimage -d $scanner --format tiff > $filename.tiff");
-		shell_exec("convert $filename.tiff $filename.pdf");
+		$scan_result = shell_exec("scanimage -d $scanner --format tiff > $filename.tiff");
+		echo "<div>Scan result: $scan_result</div>";
+		$convert_result = shell_exec("convert $filename.tiff $filename.pdf");
+		echo "<div>Convert result: $convert_result</div>";
+		// TODO: send pdf by email
 		unlink("$filename.tiff");
+		// TODO: delete pdf once sent
 	} else {
 		echo "Enter an email address";
 	}
