180cb0eadf94a3454cc9ebecf22176968ce93bab
Matthew Dooler <dooler.matthew@gmail.com>
2016-02-06 16:11:41 +0000

    form

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 echo '<h1>AirScan</h1>';
 
+echo '<form method="POST">
+	Email: <input name="email" /><br />
+	<input type="submit" value="Scan" />
+</form>';
+
 $timestamp = date('Y-m-d_H-i-s', time());
 $filename = "AirScan_" . $timestamp;
 $scanner = "hpaio:/usb/Officejet_J4500_series?serial=CN9C7D10MW052T";
